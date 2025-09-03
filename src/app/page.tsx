971c5371f3b073c3875dbc877688d1ed5c0fb7a9
Syed Muhammad Hasan Albiz <albizsyed@gmail.com>
2025-09-03 04:01:06 -0400

    Updated home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,8 +13,8 @@ export default function Home() {
 			<main>
 				<Hero />
 				<Statistics />
-				<Goal />
 				<DataSection />
+				<Goal />
 				<CommunitySpotlight />
 			</main>
 			<Footer />
