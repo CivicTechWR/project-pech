16b0181e1c3b4f06b32164ffa7b1551cb9c68c01
j2fyi <207806977+j2fyi@users.noreply.github.com>
2025-09-06 12:44:46 -0400

    feat: research page done 🚀🚀

--- src/app/research/page.tsx
+++ src/app/research/page.tsx
@@ -12,8 +12,12 @@ import { Container } from "../../components/ui/container";
 import { Heading } from "../../components/ui/heading";
 import { Text } from "../../components/ui/text";
 import { cn } from "../../lib/utils";
+import { BlockIllustration } from "./block-illustration";
+import { BuildingIllustration } from "./building-illustration";
+import { FallenBlocksIllustration } from "./fallen-blocks-illustration";
 import { ResearchHeaderCopyIllustration } from "./header-copy-illustration";
 import { HouseIllustration } from "./house-illustration";
+import { SolidHouseIllustration } from "./solid-house-illustration";
 
 const FeatureChangeCardItems: Array<FeatureChangeCardProps> = [
 	{
@@ -137,171 +141,239 @@ export default function ResearchPage() {
 				</Container>
 
 			</section>
-			<Container size="6xl" className="py-20">
-				<div className="max-w-3xl flex flex-col gap-14">
-					<section className="flex flex-col gap-8">
-						<Heading as="h3" size="lg" className="text-brand-dark-green">
-							We've Got Cracks in Our System
-						</Heading>
-						<Text size="lg" className="text-brand-grey">
-							To make sure our system does not topple over, we have to build a better foundation that supports all of our needs. We've got cracks in our system.
-						</Text>
-
-					</section>
-					<section className="flex flex-col gap-8">
-						<Heading as="p" size="md" className="text-brand-grey">
-							1 in 10 people in Waterloo Region face homelessness
-						</Heading>
-						<div className="flex gap-4">
-							{[...Array.from({ length: 10 })].map((_, index) => (
-								<HumanIllustration
-									variant={index === 0 ? "brand-dark-green" : "brand-grey"}
-									// eslint-disable-next-line react/no-array-index-key
-									key={`human-${index}`}
-								/>
-							))}
-						</div>
-						<div className="flex flex-row gap-4">
-							<div className="flex flex-row gap-2 items-center">
-								<div className="h-4 w-4 rounded-full bg-brand-dark-green"></div>
-								<Text size="md" className="text-brand-grey">
-									Homelessness
-								</Text>
-							</div>
-							<div className="flex flex-row gap-2 items-center">
-								<div className="h-4 w-4 rounded-full bg-brand-grey"></div>
-								<Text size="md" className="text-brand-grey">
-									Non-homeless
-								</Text>
-							</div>
-						</div>
-						<Text size="md" className="text-brand-grey">
-							Since 2020, chronic homelessness has grown by
-							{" "}
-							<strong>28%</strong>
-							{" "}
-							each year.
-							{" "}
-							<Link href="https://waterlooregion.org/sites/default/files/PECHKeyFindingsReport_DRAFT.pdf" className="text-brand-medium-green">LINK</Link>
-						</Text>
-						<Text size="md" className="text-brand-grey">
-							On October 22, 2024,
-							{" "}
-							<strong>2,371 people</strong>
-							{" "}
-							were experiencing homelessness in Waterloo Region. With only
-							{" "}
-							<strong>301 shelter beds</strong>
-							{" "}
-							available, many had to live outside or sleep in cars.
-							{" "}
-							<Link
-								href="https://www.regionofwaterloo.ca/en/living-here/resources/Housing-Services/137392-CS---PIT-Count-Infographic_Accessibility.pdf"
-								className="text-brand-medium-green"
-							>
-								LINK
-							</Link>
-						</Text>
-					</section>
-					<section className="flex flex-col gap-8">
-						<Heading as="p" size="md" className="text-brand-grey">
-							The top 5 reasons people said caused their experience of homelessness
-						</Heading>
-						<div className="flex flex-col gap-4">
-							<ul className="list-disc list-inside">
-								<Text as="li" className="text-brand-grey" size="xl">
-									They did not have enough income for housing
-									{" "}
-									{" "}
-									<span className="text-brand-dark-green">(44%)</span>
-								</Text>
-								<Text as="li" className="text-brand-grey" size="xl">
-									Had a conflict with their partner
-									{" "}
-									{" "}
-									<span className="text-brand-dark-green">(18%)</span>
-								</Text>
-								<Text as="li" className="text-brand-grey" size="xl">
-									Had a conflict with their landlord
-									{" "}
-									{" "}
-									<span className="text-brand-dark-green">(13%)</span>
-								</Text>
-								<Text as="li" className="text-brand-grey" size="xl">
-									Were stuck living in unfit and unsafe housing conditions
-									{" "}
-									{" "}
-									<span className="text-brand-dark-green">(12%)</span>
-								</Text>
-								<Text as="li" className="text-brand-grey" size="xl">
-									Substance use challenges
-									{" "}
-									{" "}
-									<span className="text-brand-dark-green">(10%)</span>
-								</Text>
-							</ul>
-						</div>
-					</section>
-					<section className="flex flex-col gap-8">
-						<Heading as="p" size="md" className="text-brand-grey">
-							Gender-based violence is a cause of almost half (45%) of family homelessness cases.
-						</Heading>
-						<Text size="xl" className="text-brand-grey">
-							In local research, 83% of women said they stayed in an abusive housing situation because it felt safer than being unhoused. This is called hidden homelessness.
-							{" "}
-							<Link
-								className="text-brand-medium-green"
-								href="https://caeh.ca/wp-content/uploads/WH6-%E2%80%98Dont-Tell-People-Youre-Homeless-Experiences-of-Safety-Violence-and-Justice-among-Womens-Emergency-Shelter-Users.pdf"
-							>
-								[LINK]
-							</Link>
-						</Text>
-						<Text size="xl" className="text-brand-grey">
-							Youth on the margins: a region-wide survey in 2023, found that 13% of young people had experienced homelessness.
-							{" "}
-							<Link
-								className="text-brand-medium-green"
-								href="https://childrenandyouthplanningtable.ca/wp-content/uploads/2024/08/HOMELESSNESS-Snapshot-YOUTH-IMPACT-SURVEY-Fast-Facts-V2-FINAL.pdf"
-							>
-								[LINK]
-							</Link>
-						</Text>
-					</section>
-					<section className="flex flex-col gap-8">
-						<Heading as="p" size="md" className="text-brand-grey">
-							Housing loss due to low wages and high rents
-						</Heading>
-						<div className="flex justify-between items-center">
-							<div>
-								<DollarSignIllustration />
-							</div>
-							<div>
-								<DollarSignIllustration />
-							</div>
-						</div>
-						<Text size="xl" className="text-brand-grey">
-							There is no rent control for housing built after 2018, so we are losing affordable housing. As one lived expert said: “Affordable housing means rent control. People on fixed incomes can't handle big rent increases without harming their health.”
-							{" "}
-							<Link
-								className="text-brand-medium-green"
-								href="https://caeh.ca/wp-content/uploads/WH6-%E2%80%98Dont-Tell-People-Youre-Homeless-Experiences-of-Safety-Violence-and-Justice-among-Womens-Emergency-Shelter-Users.pdf"
-							>
-								[LINK]
-							</Link>
-						</Text>
-
-					</section>
-					<section className="flex flex-col gap-8">
-						<Heading as="p" size="md" className="text-brand-grey">
-							Even with shelters and housing supports, many people can't find a safe place that meets their needs.
-						</Heading>
-						<Text size="xl" className="text-brand-grey">
-							Shelter spaces are often for specific groups, which leaves gaps for couples, 2SLGBTQ+ people, and those with pets. Rising rents, low incomes that can't keep up with the cost of living, and the loss of deeply affordable and therapeutic housing has damaged our whole housing system...
-						</Text>
-
-					</section>
+			<div className="relative overflow-hidden">
+				<div className="absolute hidden lg:block top-[-80px] right-[-550px]">
+					<BuildingIllustration
+						className="h-[1792px]"
+					/>
 				</div>
-			</Container>
+				<Container size="6xl" className="py-20 ">
+					<div className="flex flex-col gap-20">
+
+						<section className="max-w-3xl flex flex-col gap-8">
+							<Heading as="h3" size="lg" className="text-brand-dark-green">
+								We've Got Cracks in Our System
+							</Heading>
+							<Text size="lg" className="text-brand-grey">
+								To make sure our system does not topple over, we have to build a better foundation that supports all of our needs. We've got cracks in our system.
+							</Text>
+						</section>
+
+						<div className="relative">
+							<section className="max-w-3xl flex flex-col gap-8">
+								<Heading as="p" size="md" className="text-brand-grey">
+									1 in 10 people in Waterloo Region face homelessness
+								</Heading>
+								<div className="flex gap-4">
+									{[...Array.from({ length: 10 })].map((_, index) => (
+										<HumanIllustration
+											variant={index === 0 ? "brand-dark-green" : "brand-grey"}
+											// eslint-disable-next-line react/no-array-index-key
+											key={`human-${index}`}
+										/>
+									))}
+								</div>
+								<div className="flex flex-row gap-4">
+									<div className="flex flex-row gap-2 items-center">
+										<div className="h-4 w-4 rounded-full bg-brand-dark-green"></div>
+										<Text size="md" className="text-brand-grey">
+											Homelessness
+										</Text>
+									</div>
+									<div className="flex flex-row gap-2 items-center">
+										<div className="h-4 w-4 rounded-full bg-brand-grey"></div>
+										<Text size="md" className="text-brand-grey">
+											Non-homeless
+										</Text>
+									</div>
+								</div>
+								<Text size="md" className="text-brand-grey">
+									Since 2020, chronic homelessness has grown by
+									{" "}
+									<strong>28%</strong>
+									{" "}
+									each year.
+									{" "}
+									<Link href="https://waterlooregion.org/sites/default/files/PECHKeyFindingsReport_DRAFT.pdf" className="text-brand-medium-green">LINK</Link>
+								</Text>
+								<Text size="md" className="text-brand-grey">
+									On October 22, 2024,
+									{" "}
+									<strong>2,371 people</strong>
+									{" "}
+									were experiencing homelessness in Waterloo Region. With only
+									{" "}
+									<strong>301 shelter beds</strong>
+									{" "}
+									available, many had to live outside or sleep in cars.
+									{" "}
+									<Link
+										href="https://www.regionofwaterloo.ca/en/living-here/resources/Housing-Services/137392-CS---PIT-Count-Infographic_Accessibility.pdf"
+										className="text-brand-medium-green"
+									>
+										LINK
+									</Link>
+								</Text>
+							</section>
+							<BlockIllustration
+								className={cn(
+									"absolute top-0 right-35",
+									"hidden lg:block",
+									"rotate-[-17.5deg]",
+									"scale-150",
+								)}
+							/>
+						</div>
+						<div className="relative">
+							<section className="max-w-xl flex flex-col gap-8">
+								<Heading as="p" size="md" className="text-brand-grey">
+									The top 5 reasons people said caused their experience of homelessness
+								</Heading>
+								<div className="flex flex-col gap-4">
+									<ul className="list-disc list-inside">
+										<Text as="li" className="text-brand-grey" size="md">
+											They did not have enough income for housing
+											{" "}
+											{" "}
+											<span className="text-brand-dark-green">(44%)</span>
+										</Text>
+										<Text as="li" className="text-brand-grey" size="md">
+											Had a conflict with their partner
+											{" "}
+											{" "}
+											<span className="text-brand-dark-green">(18%)</span>
+										</Text>
+										<Text as="li" className="text-brand-grey" size="md">
+											Had a conflict with their landlord
+											{" "}
+											{" "}
+											<span className="text-brand-dark-green">(13%)</span>
+										</Text>
+										<Text as="li" className="text-brand-grey" size="md">
+											Were stuck living in unfit and unsafe housing conditions
+											{" "}
+											{" "}
+											<span className="text-brand-dark-green">(12%)</span>
+										</Text>
+										<Text as="li" className="text-brand-grey" size="md">
+											Substance use challenges
+											{" "}
+											{" "}
+											<span className="text-brand-dark-green">(10%)</span>
+										</Text>
+									</ul>
+								</div>
+							</section>
+							<BlockIllustration
+								className={cn(
+									"absolute top-20 right-35",
+									"hidden lg:block",
+									"rotate-[30deg]",
+									"scale-150",
+								)}
+							/>
+						</div>
+						<div className="relative">
+							<section className="max-w-xl flex flex-col gap-8">
+								<Heading as="p" size="md" className="text-brand-grey">
+									Gender-based violence is a cause of almost half (45%) of family homelessness cases.
+								</Heading>
+								<Text size="xl" className="text-brand-grey">
+									In local research, 83% of women said they stayed in an abusive housing situation because it felt safer than being unhoused. This is called hidden homelessness.
+									{" "}
+									<Link
+										className="text-brand-medium-green"
+										href="https://caeh.ca/wp-content/uploads/WH6-%E2%80%98Dont-Tell-People-Youre-Homeless-Experiences-of-Safety-Violence-and-Justice-among-Womens-Emergency-Shelter-Users.pdf"
+									>
+										[LINK]
+									</Link>
+								</Text>
+								<Text size="xl" className="text-brand-grey">
+									Youth on the margins: a region-wide survey in 2023, found that 13% of young people had experienced homelessness.
+									{" "}
+									<Link
+										className="text-brand-medium-green"
+										href="https://childrenandyouthplanningtable.ca/wp-content/uploads/2024/08/HOMELESSNESS-Snapshot-YOUTH-IMPACT-SURVEY-Fast-Facts-V2-FINAL.pdf"
+									>
+										[LINK]
+									</Link>
+								</Text>
+							</section>
+							<BlockIllustration
+								className={cn(
+									"absolute top-20 right-50",
+									"hidden lg:block",
+									"rotate-[-120deg]",
+									"scale-150",
+								)}
+							/>
+						</div>
+						<div className="relative">
+							<section className="max-w-xl flex flex-col gap-12">
+								<Heading as="p" size="md" className="text-brand-grey">
+									Housing loss due to low wages and high rents
+								</Heading>
+								<div className="flex justify-evenly items-center gap-4">
+									<div className="flex flex-col gap-2 items-center">
+										<DollarSignIllustration className="h-36" />
+										<div className="flex flex-col gap-2 items-center">
+											<Text size="md" className="text-brand-grey max-w-[50ch] text-center">
+												Ontario Disability Support Program (ODSP) housing allowance
+											</Text>
+											<Text size="md" className="text-brand-dark-green">
+												$522
+											</Text>
+										</div>
+									</div>
+									<div className="flex flex-col gap-2 items-center">
+										<SolidHouseIllustration className="h-36" />
+										<div className="flex flex-col gap-2 items-center">
+											<Text size="md" className="text-brand-grey text-center">
+												Average rent price for 1-bedrooms in Waterloo Region
+											</Text>
+											<Text size="md" className="text-brand-dark-green">
+												$1,600+/month
+											</Text>
+										</div>
+									</div>
+								</div>
+								<Text size="xl" className="text-brand-grey">
+									There is no rent control for housing built after 2018, so we are losing affordable housing. As one lived expert said: “Affordable housing means rent control. People on fixed incomes can't handle big rent increases without harming their health.”
+									{" "}
+									<Link
+										className="text-brand-medium-green"
+										href="https://caeh.ca/wp-content/uploads/WH6-%E2%80%98Dont-Tell-People-Youre-Homeless-Experiences-of-Safety-Violence-and-Justice-among-Womens-Emergency-Shelter-Users.pdf"
+									>
+										[LINK]
+									</Link>
+								</Text>
+
+							</section>
+							<BlockIllustration
+								className={cn(
+									"absolute top-20 right-30",
+									"hidden lg:block",
+									"rotate-[60deg]",
+									"scale-150",
+								)}
+							/>
+						</div>
+						<section className="flex flex-row items-center space-between">
+							<div className="flex flex-col gap-8">
+								<Heading as="p" size="md" className="text-brand-grey">
+									Even with shelters and housing supports, many people can't find a safe place that meets their needs.
+								</Heading>
+								<Text size="xl" className="text-brand-grey">
+									Shelter spaces are often for specific groups, which leaves gaps for couples, 2SLGBTQ+ people, and those with pets. Rising rents, low incomes that can't keep up with the cost of living, and the loss of deeply affordable and therapeutic housing has damaged our whole housing system...
+								</Text>
+							</div>
+							<div>
+								<FallenBlocksIllustration className="w-128 h-84" />
+							</div>
+						</section>
+					</div>
+				</Container>
+			</div>
 			<div className="bg-brand-black text-white">
 				<Container size="6xl" className="py-20 flex flex-col gap-8">
 					<div className="flex flex-col gap-8">
